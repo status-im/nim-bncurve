71671055007654d162701e0f877d6d17e9cb0a34
Jacek Sieka <jacek@status.im>
2024-12-10 15:38:02 +0100

    revert half the changes

--- bncurve/arith.nim
+++ bncurve/arith.nim
@@ -20,12 +20,13 @@ when sizeof(int) == 4:
   import stint/private/primitives/compiletime_fallback
 
   # TODO a future intops library should expose this on 32-bit platforms too!
-  func addC*(cOut: var Carry, sum: var uint64, a, b: uint64, cIn: Carry) =
+  func addC*(cOut: var Carry, sum: var uint64, a, b: uint64, cIn: Carry) {.inline.} =
     addC_nim(cOut, sum, a, b, cIn)
-  func subB*(bOut: var Borrow, diff: var uint64, a, b: uint64, bIn: Borrow) =
+  func subB*(bOut: var Borrow, diff: var uint64, a, b: uint64, bIn: Borrow) {.inline.} =
     subB_nim(bOut, diff, a, b, bIn)
-  func muladd2(hi, lo: var uint64, a, b, c1, c2: uint64) =
+  proc muladd2(hi, lo: var uint64, a, b, c1, c2: uint64) =
     muladd2_nim(hi, lo, a, b, c1, c2)
+
 else:
   import stint/private/primitives/[addcarry_subborrow, extended_precision]
 
@@ -33,227 +34,200 @@ type
   BNU256* = array[4, uint64]
   BNU512* = array[8, uint64]
 
-proc setRandom*(a: var BNU512) =
+proc setRandom*(a: var BNU512) {.inline.} =
   ## Set value of integer ``a`` to random value.
   let ret = randomBytes(a)
   doAssert(ret == 8)
 
-proc random*(t: typedesc[BNU512]): BNU512 {.noinit.} =
+proc random*(t: typedesc[BNU512]): BNU512 {.inline, noinit.} =
   ## Return random 512bit integer.
   setRandom(result)
 
-func setZero*(a: var BNU256) =
+proc setZero*(a: var BNU256) {.inline.} =
   ## Set value of integer ``a`` to zero.
   a[0] = 0'u64
   a[1] = 0'u64
   a[2] = 0'u64
   a[3] = 0'u64
 
-func setOne*(a: var BNU256) =
+proc setOne*(a: var BNU256) {.inline.} =
   ## Set value of integer ``a`` to one.
   a[0] = 1'u64
   a[1] = 0'u64
   a[2] = 0'u64
   a[3] = 0'u64
 
-func zero*(t: typedesc[BNU256]): BNU256 =
+proc zero*(t: typedesc[BNU256]): BNU256 {.inline.} =
   ## Return zero 256bit integer.
   discard
 
-func one*(t: typedesc[BNU256]): BNU256 {.noinit.} =
+proc one*(t: typedesc[BNU256]): BNU256 {.inline, noinit.} =
   ## Return one 256bit integer.
   setOne(result)
 
-func isZero*(a: BNU256): bool =
+proc isZero*(a: BNU256): bool {.inline, noinit.} =
   ## Check if integer ``a`` is zero.
   (a[0] == 0'u64) and (a[1] == 0'u64) and (a[2] == 0'u64) and (a[3] == 0'u64)
 
-func setBit*[N: static int](a: var array[N, uint64], n: int, to: bool): bool =
+proc setBit*(a: var openArray[uint64], n: int,
+             to: bool): bool {.inline, noinit.} =
   ## Set bit of integer ``a`` at position ``n`` to value ``to``.
   if n >= 256:
-    false
-  else:
-    let part = n shr 6
-    let index = n and 63
-    let value = uint64(to)
-    a[part] = a[part] and not (1'u64 shl index) or (value shl index)
-    true
+    return
+  let part = n shr 6
+  let index = n and 63
+  let value = uint64(to)
+  a[part] = a[part] and not(1'u64 shl index) or (value shl index)
+  result = true
 
-func getBit*[N: static int](a: array[N, uint64], n: int): bool =
+proc getBit*(a: openArray[uint64], n: int): bool {.inline, noinit.} =
   ## Get value of bit at position ``n`` in integer ``a``.
   let part = n shr 6
   let bit = n - (part shl 6)
-  ((a[part] and (1'u64 shl bit)) != 0)
+  result = ((a[part] and (1'u64 shl bit)) != 0)
 
-func div2(a: var BNU256) =
+proc div2(a: var BNU256) {.inline.} =
   ## Divide integer ``a`` in place by ``2``.
-  a[0] = a[0] shr 1 or a[1] shl 63
-  a[1] = a[1] shr 1 or a[2] shl 63
-  a[2] = a[2] shr 1 or a[3] shl 63
+  var t = a[3] shl 63
   a[3] = a[3] shr 1
+  let b = a[2] shl 63
+  a[2] = a[2] shr 1
+  a[2] = a[2] or t
+  t = a[1] shl 63
+  a[1] = a[1] shr 1
+  a[1] = a[1] or b
+  a[0] = a[0] shr 1
+  a[0] = a[0] or t
 
-func mul2(a: var BNU256) =
+proc mul2(a: var BNU256) {.inline.} =
   ## Multiply integer ``a`` in place by ``2``.
-  var carry: Carry
-  staticFor i, 0 ..< 4:
-    addC(carry, a[i], a[i], a[i], carry)
+  var last = 0'u64
+  for i in a.mitems():
+    let tmp = i shr 63
+    i = i shl 1
+    i = i or last
+    last = tmp
 
-func addNoCarry(a: var BNU256, b: BNU256) =
+proc addNoCarry(a: var BNU256, b: BNU256) {.inline.} =
   ## Calculate integer addition ``a = a + b``.
   var carry: Carry
-  staticFor i, 0 ..< 4:
+  staticFor i, 0, 4:
     addC(carry, a[i], a[i], b[i], carry)
 
-func subNoBorrow(a: var BNU256, b: BNU256) =
+proc subNoBorrow(a: var BNU256, b: BNU256) {.inline.} =
   ## Calculate integer substraction ``a = a - b``.
   var borrow: Borrow
-  staticFor i, 0 ..< 4:
+  staticFor i, 0, 4:
     subB(borrow, a[i], a[i], b[i], borrow)
 
-func macDigit[N, N2: static int](
-    acc: var array[N, uint64], pos: static int, b: array[N2, uint64], c: uint64
-) =
+proc macDigit[N, N2: static int](
+    acc: var array[N, uint64], pos: static int, b: array[N2, uint64], c: uint64) =
   if c == 0'u64:
     return
 
   var carry = 0'u64
 
-  staticFor i, pos ..< N:
+  staticFor i, pos, N:
     when (i - pos) < len(b):
-      muladd2(carry, acc[i], b[i - pos], c, acc[i], carry)
+      muladd2(carry, acc[i], b[i-pos], c, acc[i], carry)
     else:
       muladd2(carry, acc[i], 0, c, acc[i], carry)
 
-func macDigit[N, N2: static int](
-    acc: var array[N, uint64], pos: static int, b: static array[N2, uint64], c: uint64
-) =
-  if c == 0'u64:
-    return
-
-  var carry = 0'u64
-
-  staticFor i, pos ..< N:
-    when (i - pos) < len(b):
-      muladd2(carry, acc[i], b[i - pos], c, acc[i], carry)
-    else:
-      muladd2(carry, acc[i], 0, c, acc[i], carry)
-
-func mulReduce(a: var BNU256, by: BNU256, modulo: static BNU256, inv: static uint64) =
-  var res {.align: 32.}: array[8, uint64]
-  staticFor i, 0 ..< 4:
+proc mulReduce(a: var BNU256, by: BNU256, modulus: BNU256, inv: uint64) =
+  var res: array[4 * 2, uint64]
+  staticFor i, 0, 4:
     macDigit(res, i, by, a[i])
 
-  staticFor i, 0 ..< 4:
+  staticFor i, 0, 4:
     let k = inv * res[i]
-    macDigit(res, i, modulo, k)
+    macDigit(res, i, modulus, k)
 
-  staticFor i, 0 ..< 4:
+  staticFor i, 0, 4:
     a[i] = res[i + 4]
 
-func compare*(a: BNU256, b: BNU256): int =
+proc compare*(a: BNU256, b: BNU256): int {.noinit, inline.}=
   ## Compare integers ``a`` and ``b``.
   ## Returns ``-1`` if ``a < b``, ``1`` if ``a > b``, ``0`` if ``a == b``.
-  staticFor j, 0 ..< 4:
-    const i = 3 - j
+  for i in countdown(3, 0):
     if a[i] < b[i]:
       return -1
     elif a[i] > b[i]:
       return 1
   return 0
 
-func `<`*(a: BNU256, b: BNU256): bool =
+proc `<`*(a: BNU256, b: BNU256): bool {.noinit, inline.} =
   ## Return true if `a < b`.
-  staticFor j, 0 ..< 4:
-    const i = 3 - j
-    if a[i] < b[i]:
-      return true
-    elif a[i] > b[i]:
-      return false
-  return false
+  result = (compare(a, b) == -1)
 
-func `<=`*(a: BNU256, b: BNU256): bool =
+proc `<=`*(a: BNU256, b: BNU256): bool {.noinit, inline.} =
   ## Return true if `a <= b`.
-  staticFor j, 0 ..< 4:
-    const i = 3 - j
-    if a[i] < b[i]:
-      return true
-    elif a[i] > b[i]:
-      return false
-  return true
+  result = (compare(a, b) <= 0)
 
-func `==`*(a, b: BNU256): bool =
+proc `==`*(a: BNU256, b: BNU256): bool {.noinit, inline.} =
   ## Return true if `a == b`.
-  var res = 0'u64
-  staticFor i, 0 ..< 4:
-    res = res or (a[i] xor b[i])
-  res == 0
+  result = (compare(a, b) == 0)
 
-func mul*(a: var BNU256, b: BNU256, modulo: static BNU256, inv: static uint64) =
+proc mul*(a: var BNU256, b: BNU256, modulo: BNU256,
+          inv: uint64) {.inline.} =
   ## Multiply integer ``a`` by ``b`` (mod ``modulo``) via the Montgomery
   ## multiplication method.
   mulReduce(a, b, modulo, inv)
   if a >= modulo:
     subNoBorrow(a, modulo)
 
-func mul2*(a: var BNU256, modulo: static BNU256) =
-  ## Compute `a * 2 mod modulo`
-  mul2(a)
-  if a >= modulo:
-    subNoBorrow(a, modulo)
-
-func add*(a: var BNU256, b: BNU256, modulo: static BNU256) =
+proc add*(a: var BNU256, b: BNU256, modulo: BNU256) {.inline.} =
   ## Add integer ``b`` from integer ``a`` (mod ``modulo``).
   addNoCarry(a, b)
   if a >= modulo:
     subNoBorrow(a, modulo)
 
-func sub*(a: var BNU256, b: BNU256, modulo: static BNU256) =
+proc sub*(a: var BNU256, b: BNU256, modulo: BNU256) {.inline.} =
   ## Subtract integer ``b`` from integer ``a`` (mod ``modulo``).
   if a < b:
     addNoCarry(a, modulo)
   subNoBorrow(a, b)
 
-func neg*(a: var BNU256, modulo: static BNU256) =
+proc neg*(a: var BNU256, modulo: BNU256) {.inline.} =
   ## Turn integer ``a`` into its additive inverse (mod ``modulo``).
   if a > BNU256.zero():
     var tmp = modulo
     subNoBorrow(tmp, a)
     a = tmp
 
-func isEven*(a: BNU256): bool =
+proc isEven*(a: BNU256): bool {.inline, noinit.} =
   ## Check if ``a`` is even.
   ((a[0] and 1'u64) == 0'u64)
 
-func divrem*(
-    a: BNU512, modulo: static BNU256, reminder: var BNU256
-): Option[BNU256] {.noinit.} =
+proc divrem*(a: BNU512, modulo: BNU256, reminder: var BNU256): Option[BNU256] =
   ## Divides integer ``a`` by ``modulo``, set ``remainder`` to reminder and, if
   ## possible, return quotient smaller than the modulus.
-  var q {.align: 32.} = BNU256.zero()
+  var q = BNU256.zero()
   reminder.setZero()
-  var ok = true
+  result = some[BNU256](q)
   for i in countdown(511, 0):
     mul2(reminder)
     let ret = reminder.setBit(0, a.getBit(i))
     doAssert ret
     if reminder >= modulo:
       subNoBorrow(reminder, modulo)
-      if ok and not q.setBit(i, true):
-        ok = false
+      if result.isSome():
+        if not q.setBit(i, true):
+          result = none[BNU256]()
+        else:
+          result = some[BNU256](q)
 
-  if not ok or q >= modulo:
-    none[BNU256]()
-  else:
-    some(q)
+  if result.isSome() and result.get() >= modulo:
+    result = none[BNU256]()
 
-func into*(t: typedesc[BNU512], c1: BNU256, c0: BNU256, modulo: BNU256): BNU512 =
+proc into*(t: typedesc[BNU512], c1: BNU256,
+           c0: BNU256, modulo: BNU256): BNU512 =
   ## Return 512bit integer of value ``c1 * modulo + c0``.
-
-  staticFor i, 0 ..< 4:
-    macDigit(result, i, modulo, c1[i])
-
+  macDigit(result, 0, modulo, c1[0])
+  macDigit(result, 1, modulo, c1[1])
+  macDigit(result, 2, modulo, c1[2])
+  macDigit(result, 3, modulo, c1[3])
   var carry: Carry
-  staticFor i, 0 ..< len(result):
+  staticFor i, 0, len(result):
     when len(c0) > i:
       addC(carry, result[i], result[i], c0[i], carry)
     else:
